4fd2012ff25f5e42d58d251611e79822e1463ac6
hannah666L <hannahL2323@users.noreply.github.com>
2023-10-07 16:29:50 -0400

    creating swerve subsystem, adding drive commands

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,16 +4,23 @@
 
 package frc.robot;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
-public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-  }
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
+import swervelib.math.Matter;
+import swervelib.parser.PIDFConfig;
+
+/** Add your docs here. */
+public class Constants {
+
+    public static final double ROBOT_MASS = (45) * 0.453592; // 32lbs * kg per pound
+    public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
+    public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
+
+    public static class OperatorConstants
+    {
+
+        // Joystick Deadband
+        public static final double LEFT_X_DEADBAND = 0.1;
+        public static final double LEFT_Y_DEADBAND = 0.1;
+    }
 }
